6ded961ec20b64739a58050f33ee343fc2352d5d
shad0wsaint <vince.siu4@gmail.com>
2015-03-22 04:05:42 -0700

    Finished the auto-grade getter

--- test.py
+++ test.py
@@ -3,74 +3,133 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 
 
-def downloadGradesToTextFile():
-	driver = webdriver.Firefox()
-	driver.get("https://be.my.ucla.edu/")	
-	username = driver.find_element_by_xpath("(//input)[1]")
-	password = driver.find_element_by_xpath("(//input)[2]")	
-	#gonna store my stuff in a local file LOL I r stupid
-	username.send_keys("howdy")
-	password.send_keys("desu")
-	password.send_keys(Keys.RETURN)
-	driver.implicitly_wait(10)	
 
-	#grades1 = driver.find_element_by_id("ctl00_MainContent_studyListDataList_ctl01_gradesButton")
-	#print grades1
-	#grades1.click()	
 
+
+# def downloadGradesToTextFileAlphaVersion():
+# 	driver = webdriver.Firefox()
+# 	driver.get("https://be.my.ucla.edu/")	
+# 	username = driver.find_element_by_xpath("(//input)[1]")
+# 	password = driver.find_element_by_xpath("(//input)[2]")	
+# 	loginInfo = extractPasswords()
+# 	username.send_keys(loginInfo[0])
+# 	password.send_keys(loginInfo[1])
+# 	password.send_keys(Keys.RETURN)
+# 	try:
+# 		element = WebDriverWait(driver, 10).until(
+# 			EC.title_is("UCLA Logon")
+# 			)
+# 	except TimeoutException:
+# 		print "Timeout error"
+# 		driver.quit()
+# 		exit()
+# 	#
+# 	driver.implicitly_wait(10)	
+# 	#grades1 = driver.find_element_by_id("ctl00_MainContent_studyListDataList_ctl01_gradesButton")
+# 	#print grades1
+# 	#grades1.click()	
+
+# 	classID_list = []
+# 	for i in range(5):
+# 		classID_list.append( "ctl00_MainContent_studyListDataList_ctl0" + `(2 * i + 1)` + "_gradesButton")
+# 		print classID_list[i]	
+
+# 	for i in range(5):
+# 		grades = driver.find_element_by_id(classID_list[i])
+# 		grades.click()
+# 		driver.back()
+
+# 	driver.close()
+
+
+
+def downloadGradesToTestFile():
+	print "Getting grades from myucla"
 	classID_list = []
 	for i in range(5):
 		classID_list.append( "ctl00_MainContent_studyListDataList_ctl0" + `(2 * i + 1)` + "_gradesButton")
-		print classID_list[i]	
 
-	for i in range(5):
-		grades = driver.find_element_by_id(classID_list[i])
-		grades.click()
-		driver.back()
+	driver = webdriver.PhantomJS('C:\Users\Guava\Desktop\TestDir\phantomjs-1.9.8-windows\phantomjs-1.9.8-windows\phantomjs.exe')
+	driver.get("https://my.ucla.edu/")	
+	signin = driver.find_element_by_xpath("//*[@id='ctl00_signInLink']")
+	signin.click()
+	try:
+		element = WebDriverWait(driver, 10).until(
+			EC.title_is("UCLA Logon")
+			)
+	except TimeoutException:
+		print "Timeout error"
+		driver.quit()
+		exit()
+	username = driver.find_element_by_xpath("(//input)[1]")
+	password = driver.find_element_by_xpath("(//input)[2]")	
+	loginInfo = extractPasswords()
+	username.send_keys(loginInfo[0])
+	password.send_keys(loginInfo[1])
+	password.send_keys(Keys.RETURN)
+	try:
+		element = WebDriverWait(driver, 10).until(
+			EC.title_contains("Study List")
+			)
+	except TimeoutException:
+		print "Timeout error"
+		driver.quit()
+		exit()
+	fp_curgrades = open('grades.txt', 'w')
+	try:
+		for gradelink in classID_list:
+			grades = driver.find_element_by_id(gradelink)
+			grades.click()
+			try:
+				element = WebDriverWait(driver, 20).until(
+					EC.title_contains("Exam and Homework Grades")
+					)
+			except TimeoutException:
+				print "Timeout error"
+				driver.quit()
+				exit()
 
-	driver.close()
+			lectureName = driver.find_element_by_class_name("term_display")
+			class_title = "------------------------------------------\n%s\n------------------------------------------\n" % (lectureName.text,)
+			fp_curgrades.write(class_title)
+
+			list_tablerows = driver.find_elements(By.TAG_NAME, "tr")
+			for row in list_tablerows:
+				fp_curgrades.write(format(row.text) + "\n")
+			driver.back()
+			fp_curgrades.write("\n\n\n")
+			driver.get("https://be.my.ucla.edu/studylist.aspx")
+
+			try:
+				element = WebDriverWait(driver, 20).until(
+					EC.title_contains("Study List")
+					)
+			except TimeoutException:
+				print "Timeout error"
+				driver.quit()
+				fp_curgrades.close()
+				exit()
+	except NoSuchElementException:
+		print "Finished!"
+		driver.quit()
+		fp_curgrades.close()
+		exit()
+
+
+def extractPasswords():
+	fp = open('logininfo.txt', 'r')
+	info = []
+	info.append((fp.readline()).strip())
+	info.append((fp.readline()).strip())
+	fp.close()
+	return info
 
 
 
-#downloadGradesToTextFile()
-#WHY IT NO WORK AIYAH
-#
-classID_list = []
-for i in range(5):
-	classID_list.append( "ctl00_MainContent_studyListDataList_ctl0" + `(2 * i + 1)` + "_gradesButton")
-	print classID_list[i]	
-#driver = webdriver.Firefox()
-driver = webdriver.PhantomJS('C:\Users\Guava\Desktop\TestDir\phantomjs-1.9.8-windows\phantomjs-1.9.8-windows\phantomjs.exe')
-driver.get("https://my.ucla.edu/")	
-signin = driver.find_element_by_xpath("//*[@id='ctl00_signInLink']")
-signin.click()
 
-driver.implicitly_wait(5)	
-username = driver.find_element_by_xpath("(//input)[1]")
-password = driver.find_element_by_xpath("(//input)[2]")	
-username.send_keys("howdy")
-password.send_keys("desu")
-password.send_keys(Keys.RETURN)
-driver.implicitly_wait(10)	
-
-grades = driver.find_element_by_id(classID_list[0])
-grades.click()
-driver.implicitly_wait(20)	
-
-#perhaps adding an explicit wait would be in order here
-actualTitle = driver.current_url;
-print actualTitle
-
-actualTitle = driver.title;
-print actualTitle
-
-list_tablerows = driver.find_elements(By.TAG_NAME, "tr")
-for row in list_tablerows:
-	print format(row.text)
-#driver.back()
-#<a id="ctl00_MainContent_studyListDataList_ctl01_gradesButton" class="courseLink" href="javascript:__doPostBack('ctl00$MainContent$studyListDataList$ctl01$gradesButton','')">Grades</a>
-#<a id="ctl00_MainContent_studyListDataList_ctl03_gradesButton" class="courseLink" href="javascript:__doPostBack('ctl00$MainContent$studyListDataList$ctl03$gradesButton','')">Grades</a>
-#<a id="ctl00_MainContent_studyListDataList_ctl05_gradesButton" class="courseLink" href="javascript:__doPostBack('ctl00$MainContent$studyListDataList$ctl05$gradesButton','')">Grades</a>
-#<a id="ctl00_MainContent_studyListDataList_ctl01_gradesButton" class="courseLink" href="javascript:__doPostBack('ctl00$MainContent$studyListDataList$ctl01$gradesButton','')">Grades</a>
+if __name__ == "__main__":
+	downloadGradesToTestFile()
